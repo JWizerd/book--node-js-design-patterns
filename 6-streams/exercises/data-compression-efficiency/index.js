504bf9f289794ea9a5d3e7bde7c1a1a8ef343c44
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-08-30 17:09:00 -0600

    creating data compression efficiency algo

---
+++ 6-streams/exercises/data-compression-efficiency/index.js
@@ -0,0 +1,50 @@
+/**
+ * Write a command-line script that takes a file as input and compresses
+ * it using the different algorithms available in the zlib module (Brotli, Deflate, Gzip).
+ * You want to produce a summary table that compares the algorithm's compression time and
+ * compression efficiency.
+ *
+ * STEPS:
+ * 1. fork file compression into multiple write streams
+ * 2. calculate the time how long each compression took
+ * 3. calculate the difference in size between the original and compressed file
+ */
+
+const fs = require("fs");
+const { createBrotliCompress, createGzip, createDeflate } = require("zlib");
+const filename = process.argv[2];
+const algorithmName = process.argv[3];
+const compressFile = require("./async-compression-fork-stream");
+const algorithms = {
+  deflate: createDeflate,
+  brotli: createBrotliCompress,
+  gzip: createGzip
+}
+
+async function main () {
+  try {
+    const readStream = fs.createReadStream(filename);
+
+    if (algorithms[algorithmName]) {
+      await compressFile(
+        readStream,
+        filename,
+        algorithmName,
+        algorithms[algorithmName]
+      );
+    } else {
+      for (const algo in algorithms) {
+        await compressFile(
+          readStream,
+          filename,
+          algo,
+          algorithms[algo]
+        );
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
---
+++ 6-streams/exercises/data-compression-efficiency/monitor-stream.js
@@ -0,0 +1,24 @@
+const { PassThrough } = require("stream");
+const fs = require("fs");
+
+module.exports = (filename, algorithmName) => {
+  const monitor = new PassThrough();
+  const originalFileSize = fs.statSync(filename).size;
+  let compressedFileSize = 0;
+  const start = Date.now();
+  const stats = {
+    algorithmName
+  };
+
+  monitor.on("data", (chunk) => {
+    compressedFileSize += chunk.length
+    stats.compressionPercentage = (compressedFileSize / originalFileSize * 100);
+  });
+
+  monitor.on("finish", () => {
+    stats.timeInMs = Date.now() - start;
+    console.table(stats);
+  });
+
+  return monitor;
+}
